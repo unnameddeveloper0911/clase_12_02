37f907e4a9ff12b5f15a5a2f609c095eaa67d37d
unnameddeveloper0911 <157537963+unnameddeveloper0911@users.noreply.github.com>
2026-02-15 21:17:03 -0500

    Delete 7.py

--- 7.py
+++
@@ -1,9 +0,0 @@
-##Write a function that calculates the sum of digits of a given integer. For example, the sum of digits
-##of 123 would be 1 + 2 + 3 = 6.
-def sumaDigitos(numero):
-    total = 0
-    while numero > 0:
-        total += numero % 10
-        numero //= 10
-    return total
-print(sumaDigitos(int(input("ingrese un numero: "))))
